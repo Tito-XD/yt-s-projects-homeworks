783dccdf86c570e3a15de5815ee5f179dedcfe61
Tito Wang <35629425+Tito-XD@users.noreply.github.com>
2020-04-17 11:20:48 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,29 @@
+import java.util.*; 
+public class test {
+	
+	public static void main(String[] args){
+
+
+	Random random = new Random();
+	int realNumber = random.nextInt(100)+1;
+
+	System.out.println("请输入一个1-100的整数：");
+	int i = 0;
+	Scanner reader = new Scanner(System.in);{
+	i= reader.nextInt();
+	while(i != realNumber) 
+	if(i < realNumber) {
+		System.out.println("猜小了");
+		i= reader.nextInt();
+	}
+	else if(i > realNumber) {
+		System.out.println("猜大了");
+		i= reader.nextInt();
+	}
+	else {
+		break;
+	}
+	System.out.println("猜对了");
+	}
+	}
+}
